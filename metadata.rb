1e8154b4072d713954851dd47af0651cd6803984
Karol Drazek <karol.drazek@cognifide.com>
2016-09-05 21:38:08 +0200

    Removing JAVA version constraint

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'karol.drazek@cognifide.com'
 license 'apachev2'
 description 'Installs/Configures knotx'
 long_description 'Installs/Configures knotx'
-version '0.1.0'
+version '0.1.1'
 
-depends 'java',  '~> 1.42.0'
+depends 'java'
