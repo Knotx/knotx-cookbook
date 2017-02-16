1d98deedd81ee9cbdec38107f206670e5bf6dfb0
Karol Drazek <karol.drazek@cognifide.com>
2017-02-16 10:51:07 +0100

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'karol.drazek@cognifide.com'
 license 'apachev2'
 description 'Installs/Configures knotx'
 long_description 'Installs/Configures knotx'
-version '0.1.14'
+version '0.1.15'
 
 depends 'java'
