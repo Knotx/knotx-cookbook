2de4a10bbf30eacdd0187450ff32fd902a1e5708
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2018-04-24 20:38:48 +0200

    Inspec tests update

--- test/inspec/main_test.rb
+++ test/inspec/main_test.rb
@@ -1,12 +1,12 @@
 control 'Initial test' do
   title 'checking current state'
 
-  describe service 'knotx-main' do
+  describe service 'knotx-primary' do
     it { should be_enabled }
     it { should be_running }
   end
 
-  describe service 'knotx-main2' do
+  describe service 'knotx-secondary' do
     it { should be_enabled }
     it { should be_running }
   end
@@ -17,35 +17,35 @@ end
 control 'First init script test' do
   title 'checking instances'
 
-  describe command 'service knotx-main stop' do
+  describe command 'service knotx-primary stop' do
     its('exit_status') { should eq 0 }
   end
 
-  describe service 'knotx-main' do
+  describe service 'knotx-primary' do
     it { should_not be_running }
   end
 
-  describe service 'knotx-main2' do
+  describe service 'knotx-secondary' do
     it { should be_running }
   end
 
-  describe bash('ps aux | grep [/]opt/knotx/main/') do
+  describe bash('ps aux | grep [/]opt/knotx/primary/') do
     its('exit_status') { should eq 1 }
   end
 
-  describe bash('ps aux | grep [/]opt/knotx/main2/') do
+  describe bash('ps aux | grep [/]opt/knotx/secondary/') do
     its('exit_status') { should eq 0 }
   end
 
-  describe command 'service knotx-main start' do
+  describe command 'service knotx-primary start' do
     its('exit_status') { should eq 0 }
   end
 
-  describe service 'knotx-main' do
+  describe service 'knotx-primary' do
     it { should be_running }
   end
 
-  describe service 'knotx-main2' do
+  describe service 'knotx-secondary' do
     it { should be_running }
   end
 end
@@ -55,35 +55,35 @@ end
 control 'Second init script test' do
   title 'checking instances'
 
-  describe command 'service knotx-main2 stop' do
+  describe command 'service knotx-secondary stop' do
     its('exit_status') { should eq 0 }
   end
 
-  describe service 'knotx-main' do
+  describe service 'knotx-primary' do
     it { should be_running }
   end
 
-  describe service 'knotx-main2' do
+  describe service 'knotx-secondary' do
     it { should_not be_running }
   end
 
-  describe bash('ps aux | grep [/]opt/knotx/main/') do
+  describe bash('ps aux | grep [/]opt/knotx/primary/') do
     its('exit_status') { should eq 0 }
   end
 
-  describe bash('ps aux | grep [/]opt/knotx/main2/') do
+  describe bash('ps aux | grep [/]opt/knotx/secondary/') do
     its('exit_status') { should eq 1 }
   end
 
-  describe command 'service knotx-main2 start' do
+  describe command 'service knotx-secondary start' do
     its('exit_status') { should eq 0 }
   end
 
-  describe service 'knotx-main' do
+  describe service 'knotx-primary' do
     it { should be_running }
   end
 
-  describe service 'knotx-main2' do
+  describe service 'knotx-secondary' do
     it { should be_running }
   end
 end
